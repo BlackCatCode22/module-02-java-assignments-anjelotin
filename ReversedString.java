8c16372419f52a59810781cb56be163084faa3e1
Angelo <155435977+anjelotin@users.noreply.github.com>
2024-09-05 11:37:11 -0700

    Add files via upload

---
+++ ReversedString.java
@@ -0,0 +1,27 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class ReversedString {
+    public static void main(String[] args) {
+        // Let's a reverse a string
+        String myStrToReverse = "abcdefghijklmnopqrstuvwxyz";
+        // Output the chars of myStrToReverse
+
+        for (int i = 0; i < 7; i++){
+            System.out.println("myStrToReverse[" + i + "] is : " + myStrToReverse.charAt(i));
+        }
+        System.out.println("End of initial output");
+
+        // Create the reverse string.
+        String reversedStr = "";
+        // Output reversedStr.
+        System.out.println("reversedStr before reversion loop is: " + reversedStr);
+
+        for (int i = myStrToReverse.length()-1; i >=0; i--) {
+            char charToAdd = myStrToReverse.charAt(i);
+            reversedStr = reversedStr.concat(Character.toString(charToAdd));
+
+        }
+        // Output reversedStr.
+        System.out.println("reversedStr after reversion loop is: " + reversedStr);
+    }
+}
